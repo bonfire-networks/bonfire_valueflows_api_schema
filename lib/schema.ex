e84ef4a02dbd9ed98bfc35a2fa97828a4e944b95
Mayel de Borniol <git@mayel.space>
2024-04-01 16:26:52 +0100

    fix

--- lib/schema.ex
+++ lib/schema.ex
@@ -7,7 +7,7 @@ if Application.compile_env(:bonfire_api_graphql, :modularity) != :disabled do
     @schema_file "lib/schema.gql"
     @external_resource @schema_file
 
-    import_types(Absinthe.Type.Custom)
+    # import_types(Absinthe.Type.Custom)
 
     import_sdl(path: @schema_file)
   end
